604ae1c6a78027606b83c4fe77cb41abed473315
jackie <862355069@qq.com>
2020-03-04 08:37:21 +0000

    基础页面

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', 'about')
+@section('content')
+    <h1>about</h1>
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', 'help')
+@section('content')
+    <h1>help</h1>
+@stop
