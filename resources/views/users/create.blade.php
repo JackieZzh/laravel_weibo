d2c162c9c310c84b0047aa80192ce75f629cfd1a
jackie <862355069@qq.com>
2020-03-05 17:28:51 +0800

    中文报错提示

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -9,6 +9,8 @@
         <h5>注册</h5>
       </div>
       <div class="card-body">
+        @include('shared._errors')
+
         <form method="POST" action="{{ route('users.store') }}">
           {{ csrf_field() }}
           <div class="form-group">
