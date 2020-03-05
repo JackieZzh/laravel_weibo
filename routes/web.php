38b9207ff751b1c8de66715e29d22aadeec011ee
Jackie <jackie@JackiedeMacBook-Pro.local>
2020-03-05 13:48:08 +0800

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+Route::get('signup', 'UserController@create')->name('signup');
