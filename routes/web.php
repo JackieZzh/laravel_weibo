41e76c982c11014226d718cc92760df3966b84a9
jackie <862355069@qq.com>
2020-03-05 16:31:02 +0800

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,5 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
-Route::get('signup', 'UserController@create')->name('signup');
+Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
