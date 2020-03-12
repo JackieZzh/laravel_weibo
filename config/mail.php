e6faf36dfaf67fe0f6f0e6b1e173adb9bea5bc62
jackie <862355069@qq.com>
2020-03-12 01:02:35 +0000

    update

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', '862355069@qq.com'),
+        'name' => env('MAIL_FROM_NAME', 'Jackie'),
     ],
 
     /*
