f6f9e7972ee246d1838ed6aa9152f4d69c29c2c1
jackie <862355069@qq.com>
2020-03-17 00:47:12 +0000

    发布微博

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,7 @@ Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm'
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+
+// 微博创建/删除
+Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
