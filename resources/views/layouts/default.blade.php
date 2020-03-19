d6faeee1be7a85ea7649834f576469e7425e3560
jackie <862355069@qq.com>
2020-03-19 11:03:28 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,8 +1,21 @@
 <!doctype html>
 <head>
     <title>@yield('title') - 第二次尝试</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    @yield('content')
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="/">Weibo2 App</a>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+                <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
 </html>
